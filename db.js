652d1797105e04ac19f1894f35a6da2f6099c492
AlexanderShpilka <alexander.shpilka@gmail.com>
2020-06-17 12:22:41 +0300

    Ready for deployment

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
-const pool = new Pool({
-  user: 'postgres',
-  password: 'Freedom13$Autumn2017',
-  database: 'articles_database',
-  host: 'localhost',
-  port: 5432,
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL, // comes from heroku addons
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? prodConfig : devConfig,
+);
 
 module.exports = pool;
